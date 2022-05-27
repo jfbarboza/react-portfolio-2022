2d519e8af7e33a45224c9e070c8e890b6999890e
Jorge Barboza <jorgebarboza@jorges-mbp.lan>
2022-05-27 19:53:38 -0400

    Added project items

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,8 @@
-import { faHelicopterSymbol } from '@fortawesome/free-solid-svg-icons';
+import { faCodeBranch, faHelicopterSymbol } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useRef, useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import portImage01 from '../../assets/images/portfolio-image-01.jpg';
 import portImage02 from '../../assets/images/portfolio-image-02.jpg';
 import portImage03 from '../../assets/images/portfolio-image-03.jpg';
@@ -14,6 +15,7 @@ import 'leaflet/dist/leaflet.css';
 
 const Portfolio = ()=>{
     const [letterClass, setLetterClass] = useState('text-animate');
+
 
     useEffect(()=>{
         setTimeout(()=>{
@@ -33,31 +35,36 @@ const Portfolio = ()=>{
             'link': 'https://thecometcontent.com',
             'src': portImage02,
             'caption_title': 'The Commet Content',
-            'caption_description': 'Production company with clients like Discovery Channel, Master Card and Shutter Stock'
+            'caption_description': 'Production company with clients like Discovery Channel, Master Card and Shutter Stock',
+            'key': 2
         },
         {
             'link': 'https://monicachacin.com',
             'src': portImage03,
             'caption_title': 'Monica Chacin',
-            'caption_description': 'The number one freelancer copywriter in Spain'
+            'caption_description': 'The number one freelancer copywriter in Spain',
+            'key': 3
         },
         {
             'link': 'https://barrigonplastering.com',
             'src': portImage04,
             'caption_title': 'Barrigon Plastering',
-            'caption_description': 'Company specialize on plastering and construction'
+            'caption_description': 'Company specialize on plastering and construction',
+            'key': 4
         },
         {
             'link': 'https://porschexholman.com',
             'src': portImage05,
             'caption_title': 'Porsche Holman',
-            'caption_description': 'Car dealership stablished in different locations around US'
+            'caption_description': 'Car dealership stablished in different locations around US',
+            'key': 5
         },
         {
             'link': 'https://louisvarela.com',
             'src': portImage06,
-            'caption_title': 'Louis Varela Photography is portfolio site for a creative and dedicated photographer.',
-            'caption_description': 'Portfolio dedicated to '
+            'caption_title': 'Louis Varela Photography',
+            'caption_description': 'Portfolio site for a creative and dedicated photographer.',
+            'key': 6
         }
     ]
 
@@ -66,9 +73,13 @@ const Portfolio = ()=>{
         return (
         portfolioItems.map((item) =>{
             return(
-                <div className='project-item'>
-                    <img src={item.src} alt={item.caption_title}/>
-                </div>
+                <a href={item.link} target="_blank" key={item.key}>
+                    <div className='project-item'
+                        >
+                        <img src={item.src} alt={item.caption_title}/>
+                        <div className='title-bar'><FontAwesomeIcon icon={faCodeBranch} color="#275d16" />&nbsp;{item.caption_title}</div>
+                    </div>
+                </a>
             )
         })
         )
